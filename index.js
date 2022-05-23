a122f90166f49f517f9ce6562e550e6fdb578054
Lori Henderson <lorim95@gmail.com>
2022-05-22 21:28:54 -0400

    commit tree 54ad476fb8bc3aa9d6dcf64905ab8bc0e11017aa

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function superbowlWin(record) {
+    record.result === "W";
+    record.year === "2015";
+}
+record.find(superbowlWin);
+
+
+// function superbowlWin(record) {
+//     if (record.result) {
+//         return record.find(function(test) {
+//             test.result === "W"
+//         })
+//     } else {
+//         const resss = record.find(rec => rec.year);
+//         return resss
+//     }
+// }
+
+/*
+function matchName(drivers, str) {
+    const pikachu = drivers.filter(function(squirtle) {
+        return squirtle.name === str
+    })
+    return pikachu;
+}
+
+*/
